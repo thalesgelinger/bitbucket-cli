dcfa1ba564c8862eecf9fe80a353a4327a46b06f
Thales Gelinger <thalesgelinger@gmail.com>
2024-03-08 11:44:57 -0300

    feat: base pr create command

---
+++ cmd/pr.go
@@ -0,0 +1,34 @@
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var prCmd = &cobra.Command{
+	Use:                   "pr",
+	DisableFlagsInUseLine: true,
+	Short:                 "Manage pull request",
+
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) == 0 {
+			cmd.Help()
+			os.Exit(0)
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(prCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// prCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// prCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
